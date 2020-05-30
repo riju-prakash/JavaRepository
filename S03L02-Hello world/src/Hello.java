1d6bd10eb61d61b80103b9cd92e8e1a3c25e8ad5
LENOVO <LENOVO@Riju>
2020-05-30 17:59:25 +0530

    First commit

---
+++ S03L02-Hello world/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		// system is class, out is subclass and println is method
+
+	}
+
+}
